7c1fae13ebf64081e4a438d572bb4d52274adaf6
Michael Negus <michaelnegus1@gmail.com>
2017-07-19 10:28:57 +0100

    Currently working with the quartic potential

--- simple_attempts/MC_switch.jl
+++ simple_attempts/MC_switch.jl
@@ -10,8 +10,8 @@ function __init__()
   global kT = 1 # Boltzmann constant * Temperature
 
   # Numerical parameters
-  global δt = 0.1 # Timestep length
-  global notimesteps = 100 # Number of timesteps to run for
+  global δt = 0.01 # Timestep length
+  global notimesteps = 1000000 # Number of timesteps to run for
   global switch_regularity = 10 # Number of timesteps between each switch attempt
   global x_min = -4
   global x_max = 4.4
@@ -35,6 +35,8 @@ function simulate(potential_name)
   well_dis(well, x_pos) = x_pos - minima[well]
 
   x = real_pos(start_well, 0) # Starting position of the walker
+
+
   ΔF = [] # Array to store difference in free energy after every switch attempt
   noleft = 0 # Number of switch attempts the walker has been in left well
   switch_attempts = 0
@@ -46,25 +48,39 @@ function simulate(potential_name)
     oth_well = 1
   end
 
+
   R = randn()
   R_next = randn()
 
+  # x = 2.9272649686148524
+  # println(DU(x))
+  # x = x - (δt * DU(x)) / M + sqrt((0.5 * kT * δt) / M) * (R + R_next)
+  # println(x)
+
+
   for m = 1:notimesteps
-    println(string("x = ", string(x)))
-    println(string("cur_well = ", string(cur_well)))
-    dis = well_dis(cur_well, x) # Displacement from the current well
+    if isnan(x)
+      return("You're fuggered now")
+    end
+    # println(string("x = ", string(x)))
+    # println(string("cur_well = ", string(cur_well)))
+
     if m % switch_regularity == 0
       switch_attempts += 1
 
-      ΔU = U_shifted(real_pos(1, dis)) - U_shifted(real_pos(2, dis)) # Energy change in making the switch
-      println(" ")
-      println(ΔU)
-      println(exp(-ΔU))
-      println(" ")
+      # Recalibrate the wells, if a particle has managed to cross over the barrier
+      if (cur_well == 1 && x > 0) || (cur_well == 2 && x < 0)
+        cur_well, oth_well = oth_well, cur_well
+      end
+      
+      dis = well_dis(cur_well, x) # Displacement from the current well
+      ΔU = U_shifted(real_pos(oth_well, dis)) - U_shifted(x) # Energy change in making the switch
+      # println(" ")
+      # println(ΔU)
+      # println(exp(-ΔU))
+      # println(" ")
       if rand() <= min(1, exp(-ΔU))
-        temp = cur_well
-        cur_well = oth_well
-        oth_well = temp
+        cur_well, oth_well = oth_well, cur_well
         x = real_pos(cur_well, dis)
       end
 
@@ -74,13 +90,15 @@ function simulate(potential_name)
 
       P_left = noleft / switch_attempts
       P_right = (switch_attempts - noleft) / switch_attempts
+      # println([P_left, P_right])
       push!(ΔF, -kT * log(P_left / P_right) + U_shift)
     end
 
     x = x - (δt * DU(x)) / M + sqrt((0.5 * kT * δt) / M) * (R + R_next)
+
     R = R_next
     R_next = randn()
-    end
+  end
   # Clean up ΔF
   for j = [1:length(ΔF);]
     if ΔF[j] == Inf
@@ -89,6 +107,7 @@ function simulate(potential_name)
   end
   plt = Plots.plot([1:switch_attempts;], ΔF)
   Plots.display(plt)
+  println(last(ΔF))
 end
 
 function exact_sol(potential_name)
@@ -97,6 +116,7 @@ function exact_sol(potential_name)
   integrand(x) = exp(- U(x) / kT)
   P_left = quadgk(integrand, -1000, 0)
   P_right = quadgk(integrand, 0, 1000)
+  println([P_left, P_right])
   println(-kT * log(P_left[1] / P_right[1]))
 end
 
