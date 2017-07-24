175ffe8935096e66436d7f57c69889f1d3d707b5
Michael Negus <michaelnegus1@gmail.com>
2017-07-24 21:30:58 +0100

    Files for running simulation

---
+++ analysis/run_KT.jl
@@ -0,0 +1,3 @@
+using DataCollect 
+
+DataCollect.simulate("KT_NOTES", "/home/theory/phuhzr/Documents/URSS/data_storage", [0.001, 1.0], 0.001, [0.01, 2.0], 0.01, 10000000)
--- analysis/run_diff.jl
+++ analysis/run_diff.jl
@@ -1,3 +1,3 @@
 using DataCollect 
 
-DataCollect.simulate("DIFF_WIDTH", "/home/theory/phuhzr/Documents/URSS/data_storage", [0.01, 0.03], 0.01, [0.5, 1.0], 0.5, 1000000)
+DataCollect.simulate("DIFF_WIDTH", "/home/theory/phuhzr/Documents/URSS/data_storage", [0.001, 1.0], 0.001, [0.01, 2.0], 0.01, 10000000)
